41101e7331a22fd352cd133258a38b1af45c4edd
Nicolas Andres Moreno <namoreno19@misena.edu.co>
2020-07-08 18:59:01 -0500

    Configuracion de rutas en el archivo config.php

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 // define("SERVERURL","http://localhost:8080/licita/");
 date_default_timezone_set("America/Bogota");
-define("SERVERURL", "http://" . $_SERVER["HTTP_HOST"] . "/licita/");
+define("SERVERURL", "http://" . $_SERVER["HTTP_HOST"] . "/Licitaciones/");
 
-define("FOLD_PROY", $_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/Licita/");
+define("FOLD_PROY", $_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/Licitaciones/");
 define("FOLDER_TEMPLATE", FOLD_PROY . "template/");
 define("FOLDER_VIEWS", FOLD_PROY . "app/views/");
 define("URL_Us_Nue", FOLD_PROY . "app/views/Usuarios/");
 
 // assets
-define("URL_PROY", "/Licita/");
+define("URL_PROY", "/Licitaciones/");
 define("URL_CSS", URL_PROY . "assets/css/");
 define("URL_FONTS", URL_PROY . "assets/fonts/");
 define("URL_IMG", URL_PROY . "assets/img/");
